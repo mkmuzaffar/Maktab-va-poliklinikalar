637b1d2ee872fd0250295993e30be316a306dbee
mkmuzaffar <mkmatchanov@gmail.com>
2024-12-14 16:31:59 +0500

    Delete layers directory

--- layers/poliklinikanuqtalar_5.js
+++
@@ -1,1 +0,0 @@
-var json_poliklinikanuqtalar_5 = {"type":"FeatureCollection","name":"poliklinikanuqtalar_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
